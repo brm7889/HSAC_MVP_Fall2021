b4c2e847270ce7672c8188ee49907373c6eb7313
brm7889 <60950286+brm7889@users.noreply.github.com>
2021-10-05 19:27:10 -0400

    Add files via upload

--- Lahman_MVP.R
+++ Lahman_MVP.R
@@ -1,4 +1,5 @@
-#install.packages("Lahman")
+# install.packages("Lahman")
+library(tidyverse)
 
 # Load Lahman package
 library(Lahman)
@@ -11,8 +12,8 @@ View(AwardsPlayers)
 mvps <- AwardsPlayers[AwardsPlayers$awardID=="Most Valuable Player", ]
 
 # Split by league
-mvps_al <- mvps[mvps$lgID=="AL", ]
-mvps_nl <- mvps[mvps$lgID=="NL", ]
+# mvps_al <- mvps[mvps$lgID=="AL", ]
+# mvps_nl <- mvps[mvps$lgID=="NL", ]
 
 # According to Baseball Reference, "The current incarnation of the MVP award was established in 1931." so we could
 # probably start the data there
@@ -20,4 +21,14 @@ mvps_nl <- mvps[mvps$lgID=="NL", ]
 #### Get cross-refernce IDS
 data(People)
 View(People)
+
+
+chadwick <- read_csv("chadwick.csv")
+lahman_people <- read_csv("lahman_people.csv")
+
+### Merge with lahman people
+mvps <- merge(mvps, lahman_people, by="playerID")
+mvps <- merge(mvps, chadwick, by.x="bbrefID", by.y="key_bbref")
+
+
 

3ec0f7c12d8302f75b34ef5925ec691e2a7498ee
brm7889 <60950286+brm7889@users.noreply.github.com>
2021-11-02 19:13:53 -0400

    Update Lahman_MVP.R

--- Lahman_MVP.R
+++ Lahman_MVP.R
@@ -25,10 +25,42 @@ View(People)
 
 chadwick <- read_csv("chadwick.csv")
 lahman_people <- read_csv("lahman_people.csv")
+fan_hitters <- read_csv("All Hitters Since 1911.csv")
+
+fan_hitters <- fan_hitters %>% rename(yearID = Season)
+fan_hitters <- fan_hitters %>% rename(key_fangraphs = playerid)
+
+
+
+fan_pichers <- read_csv("All Pitchers Since 1911.csv")
+
+
 
 ### Merge with lahman people
 mvps <- merge(mvps, lahman_people, by="playerID")
+
+# Merge in chadwick
 mvps <- merge(mvps, chadwick, by.x="bbrefID", by.y="key_bbref")
+
+# Merge in hitters 
+mvps_hitters <- merge(mvps, fan_hitters, by=c("key_fangraphs", "yearID"), all=TRUE)
+
+mvps_hitters <- mvps_hitters[order(-mvps_hitters$WAR),]
+
+
+glimpse(mvps_hitters)
+mvps_hitters$WAR <- as.numeric(mvps_hitters$WAR)
+mvps_hitters$mvp <- (mvps_hitters$awardID == "Most Valuable Player")
+if (mvps_hitters$mvp.isna()==TRUE) {
+  mvps_hitters$mvp <- FALSE 
+}
+
+
+
+# Merge in pitchers
+mvps_pitchers <- 
+
+
 
 
 
